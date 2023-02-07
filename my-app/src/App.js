e5a763c7056b5612d71ce67ffb239b7d6930dc0e
Mercy <116111819+MercyAz@users.noreply.github.com>
2023-02-07 22:48:33 +0000

    Initial setup of React

---
+++ my-app/src/App.js
@@ -0,0 +1,17 @@
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Hero from './components/Hero';
+import Portfolio from './components/Portfolio';
+
+function App() {
+  return (
+    <div>
+    <header />
+    <footer />
+    <hero />
+    <portfolio />
+    </div>
+  );
+}
+
+export default App;
